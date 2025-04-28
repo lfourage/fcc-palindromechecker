5e5c7f91078947dd9ab0c69534bc3f7f868a5775
lfourage <ludogriph@gmail.com>
2025-04-28 12:11:25 +0200

    Nothing tested yet, html requirements met, js structure done + some functions

---
+++ script.js
@@ -0,0 +1,26 @@
+const InputStr = document.getElementById('text-input');
+const checkBtn = document.getElementById('check-btn');
+const result = document.getElementById('result');
+
+const emptyInputError = () => {};
+const getInputStr = () => {};
+const cleanInputStr = (str) => {};
+
+const reverseStr = (str) => {
+    let reversedStr = "";
+
+    for (const i = 0; i < str.length; i++) {
+        reverseStr[i] = str[str.length - 1 - i];
+    }
+    return reverseStr;
+};
+
+const printResult = (isPalindrome) => {};
+
+const checkPalindrome = () => {
+    const str = cleanInputStr(getInputStr());
+
+    printResult(str === reverseStr(str));
+};
+
+checkBtn.addEventListener("click", checkPalindrome);
