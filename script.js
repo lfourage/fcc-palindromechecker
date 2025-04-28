315cd04bca90f85f01af0a1abfdaf04e36a89bee
lfourage <ludogriph@gmail.com>
2025-04-28 14:58:46 +0200

    validated on fcc

--- script.js
+++ script.js
@@ -1,49 +1,53 @@
-const textInput = document.getElementById('text-input');
-const checkBtn = document.getElementById('check-btn');
-const result = document.getElementById('result');
+const textInput = document.getElementById("text-input");
+const checkBtn = document.getElementById("check-btn");
+const result = document.getElementById("result");
 
-const emptyInputError = () => {
-    alert('Please input a value');
-    return "";
+const emptyInputAlert = () => {
+  alert("Please input a value");
+  result.innerText = "";
+  return "";
 };
 
 const getInputStr = () => {
-    const str = textInput.value;
-    
-    return str ? str : emptyInputError();
+  const str = textInput.value;
+
+  return str;
 };
 
 const cleanInputStr = (str) => {
-    const cleaned = str;
+  const cleaned = str.toLowerCase();
+  const regex = /[^a-z0-9]/g;
 
-    if (!str)
-        return "";
-    return cleaned;
+  if (!str) return "";
+  return cleaned.replace(regex, "");
 };
 
 const reverseStr = (str) => {
-    let reversedStr = "";
+  let reversedStr = "";
 
-    for (let i = 0; i < str.length; i++) {
-        reversedStr += str[str.length - 1 - i];
-    }
-    return reversedStr;
+  for (let i = 0; i < str.length; i++) {
+    reversedStr += str[str.length - 1 - i];
+  }
+  return reversedStr;
 };
 
 const printResult = (isPalindrome) => {
-    result.innerText = `${textInput.value} is${isPalindrome ? "" : " not"} a palindrome`;
+  result.innerText = `
+        ${textInput.value.trim()} is${isPalindrome ? "" : " not"} a palindrome
+    `;
 };
 
 const resetTextInput = () => {
-    textInput.value = "";
+  textInput.value = "";
 };
 
 const checkInput = () => {
-    const str = cleanInputStr(getInputStr());
+  if (!textInput.value) return emptyInputAlert();
 
-    if (str)
-        printResult(str === reverseStr(str));
-    resetTextInput();
+  const str = cleanInputStr(getInputStr());
+
+  printResult(str === reverseStr(str));
+  resetTextInput();
 };
 
 checkBtn.addEventListener("click", checkInput);
