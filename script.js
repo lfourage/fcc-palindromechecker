78212d5e3cf666a3961bc5f7ec1739810c46ad76
lfourage <ludogriph@gmail.com>
2025-04-28 13:52:52 +0200

    palindrome detection is working, input strings not cleaned yet

--- script.js
+++ script.js
@@ -1,26 +1,49 @@
-const InputStr = document.getElementById('text-input');
+const textInput = document.getElementById('text-input');
 const checkBtn = document.getElementById('check-btn');
 const result = document.getElementById('result');
 
-const emptyInputError = () => {};
-const getInputStr = () => {};
-const cleanInputStr = (str) => {};
+const emptyInputError = () => {
+    alert('Please input a value');
+    return "";
+};
+
+const getInputStr = () => {
+    const str = textInput.value;
+    
+    return str ? str : emptyInputError();
+};
+
+const cleanInputStr = (str) => {
+    const cleaned = str;
+
+    if (!str)
+        return "";
+    return cleaned;
+};
 
 const reverseStr = (str) => {
     let reversedStr = "";
 
-    for (const i = 0; i < str.length; i++) {
-        reverseStr[i] = str[str.length - 1 - i];
+    for (let i = 0; i < str.length; i++) {
+        reversedStr += str[str.length - 1 - i];
     }
-    return reverseStr;
+    return reversedStr;
 };
 
-const printResult = (isPalindrome) => {};
+const printResult = (isPalindrome) => {
+    result.innerText = `${textInput.value} is${isPalindrome ? "" : " not"} a palindrome`;
+};
+
+const resetTextInput = () => {
+    textInput.value = "";
+};
 
 const checkInput = () => {
     const str = cleanInputStr(getInputStr());
 
-    printResult(str === reverseStr(str));
+    if (str)
+        printResult(str === reverseStr(str));
+    resetTextInput();
 };
 
 checkBtn.addEventListener("click", checkInput);
