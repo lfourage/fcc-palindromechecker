511761695bc5bb378ae15b6235fa10e3bc78d4f1
lfourage <ludogriph@gmail.com>
2025-05-20 16:54:11 +0200

    replace not working as intended

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const textInput = document.getElementById("text-input");
 const checkBtn = document.getElementById("check-btn");
 const result = document.getElementById("result");
-const specialCharacter = [
+const regexList = [
+  { regex: /[^a-z0-9]/g, replacer: ''},
   { regex: /ç/g, replacer: 'c'},
-  { regex: /é||è||ê||ë/g, replacer: 'e'},
-  { regex: /à||â||ä/g, replacer: 'a'}
+  { regex: /é|è|ê|ë/g, replacer: 'e'},
+  { regex: /à|â|ä/g, replacer: 'a'}
 ];
 
 const emptyInputAlert = () => {
@@ -14,13 +15,10 @@ const emptyInputAlert = () => {
 };
 
 const cleanInputStr = (str) => {
-  const cleaned = str.toLowerCase();
-  const regex = /[^a-z0-9]/g;
+  let cleaned = str.toLowerCase();
 
-  cleaned.replace(regex, "");
-  for (let i = 0; i < specialCharacter.length; i++) {
-    cleaned.replace(specialCharacter[i].regex, specialCharacter[i].replacer);
-  }
+  regexList.forEach(el => cleaned = cleaned.replace(el.regex, el.replacer));
+  console.log(cleaned);
   return cleaned;
 };
 
